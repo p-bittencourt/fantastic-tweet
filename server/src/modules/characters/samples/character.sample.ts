2077ccd2c2a2f966113e731a75c7f600055a7ca1
pedro <pedromonteirobittencourt@gmail.com>
2025-01-28 15:46:44 -0300

    feat: added base system modules

---
+++ server/src/modules/characters/samples/character.sample.ts
@@ -0,0 +1,68 @@
+import { ICharacter } from '../types/character.type';
+
+export const sampleCharacters: ICharacter[] = [
+  {
+    id: 'stark1',
+    name: 'Tony Stark',
+    universe: 'Marvel Cinematic Universe',
+    traits: ['Genius', 'Sarcastic', 'Innovative', 'Bold'],
+    imageUrl:
+      'https://media.wired.com/photos/59344c46bef1fc4e58f92253/master/w_2560%2Cc_limit/im_large_660.jpg',
+  },
+  {
+    id: 'khaleesi1',
+    name: 'Daenerys Targaryen',
+    universe: 'Game of Thrones',
+    traits: ['Determined', 'Strategic', 'Fierce', 'Ambitious'],
+    imageUrl:
+      'https://s2.glbimg.com/Ll9cRLD_wJxYAjNRuzMMZYLtCKY=/e.glbimg.com/og/ed/f/original/2016/06/22/daenerys.jpg',
+  },
+  {
+    id: 'skywalker1',
+    name: 'Luke Skywalker',
+    universe: 'Star Wars',
+    traits: ['Hopeful', 'Brave', 'Idealistic', 'Powerful'],
+    imageUrl:
+      'https://i.pinimg.com/474x/9e/af/0f/9eaf0fb7905b24a030c8db77f6a3bd17.jpg',
+  },
+  {
+    id: 'hermione1',
+    name: 'Hermione Granger',
+    universe: 'Harry Potter',
+    traits: ['Intelligent', 'Determined', 'Logical', 'Loyal'],
+    imageUrl:
+      'https://s2-quem.glbimg.com/12xfyjThBXTxTTYf1LfTw_VowfM=/0x0:1400x950/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2024/X/z/xNu3X5T9eQICPOxM88Ag/hemrione.jpg',
+  },
+  {
+    id: 'batman1',
+    name: 'Bruce Wayne',
+    universe: 'DC Comics',
+    traits: ['Strategic', 'Wealthy', 'Mysterious', 'Dedicated'],
+    imageUrl:
+      'https://i.pinimg.com/736x/48/51/60/485160c55c513f8eac38027385f38a52.jpg',
+  },
+  {
+    id: 'lara1',
+    name: 'Lara Croft',
+    universe: 'Tomb Raider',
+    traits: ['Adventurous', 'Athletic', 'Clever', 'Resourceful'],
+    imageUrl:
+      'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2022/05/20/lara-croft-1h7gdz69qqn90.jpg',
+  },
+  {
+    id: 'sherlock1',
+    name: 'Sherlock Holmes',
+    universe: 'Sherlock',
+    traits: ['Analytical', 'Observant', 'Eccentric', 'Brilliant'],
+    imageUrl:
+      'https://m.media-amazon.com/images/I/612zFAYKaqL._AC_UF894,1000_QL80_.jpg',
+  },
+  {
+    id: 'wonder1',
+    name: 'Diana Prince',
+    universe: 'DC Comics',
+    traits: ['Compassionate', 'Warrior', 'Leader', 'Diplomatic'],
+    imageUrl:
+      'https://www.ligadoemviagem.com.br/wp-content/uploads/2017/06/Cr%C3%ADtica-Filme-Mulher-Maravilha-Wonder-Woman-2017-Poster.jpg',
+  },
+];
---
+++ server/src/modules/characters/types/character.type.ts
@@ -0,0 +1,7 @@
+export interface ICharacter {
+  id: string;
+  name: string;
+  universe: string;
+  traits: string[];
+  imageUrl: string;
+}
